7c81971e58f176a6eafc01fde86660753cb0af90
w1s_sa <bwissal02@gmail.com>
2025-11-28 21:11:57 +0100

    added minikube ip

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+# terraform.tfvars
+namespace   = "notesapp"
+minikube_ip = "192.168.49.2"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,12 @@ variable "vm_ip" {
   description = "Adresse IP de la VM pour nip.io"
   type        = string
 }
+variable "minikube_ip" {
+  description = "IP address of Minikube"
+  type        = string
+}
+variable "ingress_host" {
+  description = "Host for the ingress resource"
+  type        = string
+  default     = "notes.${var.minikube_ip}.nip.io"
+}
