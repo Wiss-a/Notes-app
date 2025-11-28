368cb3f8f70f29448859aa938e1370b7a85a30c0
w1s_sa <bwissal02@gmail.com>
2025-11-28 21:00:09 +0100

    changed

--- terraform/k8s/namespace.tf
+++ terraform/k8s/namespace.tf
@@ -1,3 +1,7 @@
+variable "namespace" {
+  description = "Kubernetes namespace for the NotesApp"
+  default     = "notesapp"
+}
 resource "kubernetes_namespace" "notes" {
   metadata {
     name = var.namespace
