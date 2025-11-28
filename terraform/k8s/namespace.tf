07c8adaa39715bf743034687d22c5a7c46ebbb45
w1s_sa <bwissal02@gmail.com>
2025-11-28 21:43:22 +0100

    deleted duplicate namespace

--- terraform/k8s/namespace.tf
+++ terraform/k8s/namespace.tf
@@ -1,7 +1,3 @@
-variable "namespace" {
-  description = "Kubernetes namespace for the NotesApp"
-  default     = "notesapp"
-}
 resource "kubernetes_namespace" "notesapp" {
   metadata {
     name = var.namespace
