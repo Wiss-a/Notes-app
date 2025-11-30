9855d0f664e9158d855032292b211de81d6f9a42
w1s_sa <bwissal02@gmail.com>
2025-11-30 11:57:05 +0100

    path changed

--- terraform/k8s/ingress.tf
+++ terraform/k8s/ingress.tf
@@ -2,8 +2,10 @@ resource "kubernetes_ingress_v1" "notes_ingress" {
   metadata {
     name      = "notes-ingress"
     namespace = kubernetes_namespace.notesapp.metadata[0].name
+
     annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
+      "kubernetes.io/ingress.class"                = "nginx"
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/$2"
     }
   }
 
@@ -12,6 +14,9 @@ resource "kubernetes_ingress_v1" "notes_ingress" {
       host = "notes.${var.minikube_ip}.nip.io"
 
       http {
+        # ---------------------------
+        # FRONTEND ROUTE (/)
+        # ---------------------------
         path {
           path      = "/"
           path_type = "Prefix"
@@ -21,6 +26,23 @@ resource "kubernetes_ingress_v1" "notes_ingress" {
               name = "notes-frontend"
               port {
                 number = 80
+              }
+            }
+          }
+        }
+
+        # ---------------------------
+        # BACKEND ROUTE (/api/*)
+        # ---------------------------
+        path {
+          path      = "/api(/|$)(.*)"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "notes-api"
+              port {
+                number = 5000
               }
             }
           }
