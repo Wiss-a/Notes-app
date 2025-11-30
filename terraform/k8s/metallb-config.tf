2e3d168606d4bd89b2f2df3a439c0aa99c46f6d4
w1s_sa <bwissal02@gmail.com>
2025-11-30 13:21:59 +0100

    added for External ip

---
+++ terraform/k8s/metallb-config.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_config_map" "metallb_config" {
+  metadata {
+    name      = "notes-metallb"
+    namespace = kubernetes_namespace.notesapp.metadata[0].name
+  }
+
+  data = {
+    config = <<-EOT
+      address-pools:
+      - name: default
+        protocol: layer2
+        addresses:
+        - 192.168.49.100-192.168.49.110
+    EOT
+  }
+}
