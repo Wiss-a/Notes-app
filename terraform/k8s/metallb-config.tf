7bbda3d234de54ef46d4f0cc483ba7864412513f
w1s_sa <bwissal02@gmail.com>
2025-11-30 13:26:43 +0100

    hi

--- terraform/k8s/metallb-config.tf
+++ terraform/k8s/metallb-config.tf
@@ -5,12 +5,12 @@ resource "kubernetes_config_map" "metallb_config" {
   }
 
   data = {
-    config =
+    config = <<-EOT
       address-pools:
       - name: default
         protocol: layer2
         addresses:
         - 192.168.49.100-192.168.49.110
-    
+    EOT
   }
 }
